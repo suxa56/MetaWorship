ce6fa6f648c661456c82d5bec5020c6c6b91c2d5
suxa <suxa5637@mail.ru>
2023-05-22 17:18:02 +0500

    tonality list

--- app/src/main/java/uz/suxa/metaworship/presentation/NewSongFragment.kt
+++ app/src/main/java/uz/suxa/metaworship/presentation/NewSongFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.lifecycle.ViewModelProvider
 import uz.suxa.metaworship.databinding.FragmentNewSongBinding
 
@@ -24,16 +26,21 @@ class NewSongFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentNewSongBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeLiveData()
+    }
 
+    private fun observeLiveData() {
+        viewModel.tonalityList.observe(viewLifecycleOwner) {
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, it)
+            (binding.songTonalityTil.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/uz/suxa/metaworship/presentation/SongViewModel.kt
+++ app/src/main/java/uz/suxa/metaworship/presentation/SongViewModel.kt
@@ -2,6 +2,8 @@ package uz.suxa.metaworship.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import uz.suxa.metaworship.data.SongRepoImpl
@@ -15,16 +17,31 @@ class SongViewModel(application: Application) : AndroidViewModel(application) {
 
     private val addSongUseCase = AddSongUseCase(repo)
 
-    fun addSong(title: String, lyrics: String, chords: String, tonality: Tonality) {
-        val song = SongModel(
-            title = title,
-            lyrics = lyrics,
-            chords = chords,
-            defaultTonality = tonality
-        )
+    private val _tonalityList = MutableLiveData<List<String>>()
+    val tonalityList: LiveData<List<String>> get() = _tonalityList
 
+    init {
+        fillTonalityList()
+    }
+
+    fun addSong(title: String, lyrics: String, chords: String, tonality: Tonality) =
         viewModelScope.launch {
+            val song = SongModel(
+                title = title,
+                lyrics = lyrics,
+                chords = chords,
+                defaultTonality = tonality
+            )
             addSongUseCase(song)
         }
+
+    private fun fillTonalityList() {
+        _tonalityList.value = listOf(
+            "C", "C#", "Db", "D", "D#", "Eb", "E", "F",
+            "Gb", "G", "G#", "Ab", "A", "A#", "B/Hb", "H"
+        )
     }
+
+
+
 }
